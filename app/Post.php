0a597bd73177fb92c36089951468c1147755666b
kefirm <kefirm@tlen.pl>
2020-02-19 15:48:50 +0100

    sad

--- app/Post.php
+++ app/Post.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
+    protected $guarded = []; //everything is ok, do not guard
+
     public function user()
     {
         return $this->belongsTo(User::class);
--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -9,10 +9,10 @@
         <div class="col-9 pt-5">
             <div class="d-flex justify-content-between align-items-baseline">
                 <h1>{{$user->username}}</h1>
-                <a href="#">Add new post</a>
+                <a href="/p/create">Add new post</a>
             </div>
             <div class="d-flex">
-                <div class="pr-5"><strong>153</strong> posts</div>
+                <div class="pr-5"><strong>{{$user->posts->count()}}</strong> posts</div>
                 <div class="pr-5"><strong>23k</strong> followers</div>
                 <div class="pr-5"><strong>212</strong> following</div>
             </div>
@@ -22,15 +22,12 @@
         </div>
     </div>
     <div class="row pt-5">
-        <div class="col-4">
-            <img src="https://scontent-waw1-1.cdninstagram.com/v/t51.2885-15/sh0.08/e35/c0.106.921.921a/s640x640/84770545_2609265086061431_6387769783591162166_n.jpg?_nc_ht=scontent-waw1-1.cdninstagram.com&_nc_cat=111&_nc_ohc=wzD-rtA33d0AX8xnp42&oh=21c16c65d1cd9e5ef85795823cc7b050&oe=5EC4FDED" class="w-100">
-        </div>
-        <div class="col-4">
-            <img src="https://scontent-waw1-1.cdninstagram.com/v/t51.2885-15/sh0.08/e35/c1.0.748.748a/s640x640/82339013_604012433755999_687788727040640519_n.jpg?_nc_ht=scontent-waw1-1.cdninstagram.com&_nc_cat=107&_nc_ohc=Z0xswacDMToAX_6fzPX&oh=b67e7cc00a7eaf378549b0baa7eee11f&oe=5EFC0CA4" class="w-100">
-        </div>
-        <div class="col-4">
-            <img src="https://scontent-waw1-1.cdninstagram.com/v/t51.2885-15/e35/c95.0.560.560a/83685533_642392923240008_2402831147948054363_n.jpg?_nc_ht=scontent-waw1-1.cdninstagram.com&_nc_cat=100&_nc_ohc=fdIABezXCmMAX8wHhfY&oh=e4f6c7a988d945edaad103b5b798348a&oe=5EFC6073" class="w-100">
-        </div>
+        @foreach($user->posts as $post)
+            <div class="col-4 pb-4">
+                <a href="/p/{{$post->id}}"><img src="/storage/{{$post->image}}" class="w-100"></a>
+            </div>
+        @endforeach
+
     </div>
 </div>
 @endsection
